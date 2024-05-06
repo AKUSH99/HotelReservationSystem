9d3f652d6b010a8d92fe1d934d5adb59933d9ec4
AKUSH99 <106559584+AKUSH99@users.noreply.github.com>
2024-05-06 10:50:10 +0200

    city = str(input("Enter city: "))         max_guest = int(input("Enter max guests: "))         stars = int(input("Enter stars 1 to 5 (optional): "))         hotel_city_max_guest_stars = sm.get_hotels_by_city_room_guest_stars(city=city, max_guest=max_guest, stars=stars)         for hotel in hotel_city_max_guest_stars:             print(hotel)

--- business/SearchManager.py
+++ business/SearchManager.py
@@ -44,8 +44,8 @@ if __name__ == '__main__':
             hotels = self.__session.execute(query).scalars().all()
             return hotels
 
-        def get_hotels_by_city_room_guest_stars(self, city, room, num_guest, stars):
-            query = select(Hotel).join(Address).join(Booking).join(Room).where((Address.city.like(f"%{city}%") & (Hotel.stars == stars) & (Room.max_guests == num_guest)))
+        def get_hotels_by_city_room_guest_stars(self, city, max_guest, stars):
+            query = select(Hotel).join(Hotel.address).join(Room.max_guests).where(Address.city.like(f"%{city}%") & (Hotel.stars == stars) & (Room.max_guests == max_guest))
             hotels = self.__session.execute(query).scalars().all()
             return hotels
 
@@ -57,34 +57,34 @@ if __name__ == '__main__':
             print(hotel)
 
 
-        # User Story 1.1.1.
-        # Ich möchte alle Hotels in einer Stadt durchsuchen, damit ich
-        # das Hotel nach meinem bevorzugten Standort (Stadt) auswählen kann.
-        city = input("Enter city: ")
-        hotel_city = sm.get_hotels_by_city(city)
-        for hotel in hotel_city:
-            print(hotel)
-
-
-        # User Story 1.1.2
-        # Ich möchte alle Hotels in einer Stadt nach der Anzahl der
-        # Sterne durchsuchen.
-        city = str(input("Enter city: "))
-        stars = int(input("Enter stars 1 to 5: "))
-        hotel_stars = sm.get_hotels_by_city_and_stars(city=city, stars=stars)
-        if not hotel_stars and stars == stars:
-            print("No hotels found with given amount of stars.! ")
-        else:
-            for hotel in hotel_stars:
-                print(hotel)
+        # # User Story 1.1.1.
+        # # Ich möchte alle Hotels in einer Stadt durchsuchen, damit ich
+        # # das Hotel nach meinem bevorzugten Standort (Stadt) auswählen kann.
+        # city = input("Enter city: ")
+        # hotel_city = sm.get_hotels_by_city(city)
+        # for hotel in hotel_city:
+        #     print(hotel)
+        #
+        #
+        # # User Story 1.1.2
+        # # Ich möchte alle Hotels in einer Stadt nach der Anzahl der
+        # # Sterne durchsuchen.
+        # city = str(input("Enter city: "))
+        # stars = int(input("Enter stars 1 to 5: "))
+        # hotel_stars = sm.get_hotels_by_city_and_stars(city=city, stars=stars)
+        # if not hotel_stars and stars == stars:
+        #     print("No hotels found with given amount of stars.! ")
+        # else:
+        #     for hotel in hotel_stars:
+        #         print(hotel)
 
 
         # 1.1.3. Ich möchte alle Hotels in einer Stadt durchsuchen, die Zimmer
         # haben, die meiner Gästezahl entsprechen (nur 1 Zimmer pro
         # Buchung), entweder mit oder ohne Anzahl der Sterne.
         city = str(input("Enter city: "))
-        max_guests = int(input("Enter max guests: "))
+        max_guest = int(input("Enter max guests: "))
         stars = int(input("Enter stars 1 to 5 (optional): "))
-        hotel_city_maxguest_stars = sm.get_hotels_by_city_room_guest_stars(city=city, num_guest=max_guests, stars=stars, room=0)
-        for hotel in hotel_city_maxguest_stars:
+        hotel_city_max_guest_stars = sm.get_hotels_by_city_room_guest_stars(city=city, max_guest=max_guest, stars=stars)
+        for hotel in hotel_city_max_guest_stars:
             print(hotel)
