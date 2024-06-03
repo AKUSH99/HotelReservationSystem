447890eb7f37a73315d787563aa363635dcf18a1
sujani-ragumar <160474567+sujani-ragumar@users.noreply.github.com>
2024-06-03 11:55:26 +0200

    Implement enhanced hotel and room details retrieval functionality
    - Added SQL query to fetch detailed hotel information including name, full address, and star rating, joining Hotel and Address tables.
    - Implemented get_hotel_details method in SearchManager class to retrieve and return hotel details as a list of dictionaries, optionally filtered by hotel_id.
    - Updated SQL query for finding available rooms to include checks for existing bookings within a specified date range, ensuring accurate availability status.
    - Enhanced find_available_rooms method in SearchManager to utilize improved SQL query, returning a list of available rooms with comprehensive details including room number, type, max guests, description, amenities, and pricing.
    - Corrected imports for SQLAlchemy's and_ and or_ functions to support logical conditions in room availability checks.

---
+++ ui/RegistrationUI.py
@@ -0,0 +1,70 @@
+import sys
+
+from pathlib import Path
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from data_access.data_base import init_db
+from business.UserManager import UserManager
+
+class RegistrationUI:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    db_file = "../data/test.db"
+    database_path = Path(db_file)
+    if not database_path.is_file():
+        init_db(db_file, generate_example_data=True)
+    session = scoped_session(sessionmaker(bind=create_engine(f"sqlite:///{database_path}", echo=False)))
+    user_manager = UserManager(session)
+
+    print("USERSTORY: Login")
+    while user_manager.has_attempts_left():
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        if user_manager.login(username, password):
+            print("Login successful!")
+            break
+        else:
+            print("Login failed! Try again!")
+
+    if user_manager.get_current_login():
+        print(f"Welcome {user_manager.get_current_login().username}")
+    else:
+        print("No attempts left, program is closed!")
+        sys.exit(1)
+
+    user_manager.logout()
+    print("Goodbye!")
+    print(user_manager.get_current_login())
+
+    print("USERSTORY: Register Guest")
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    firstname = input("Enter your fristname: ")
+    lastname = input("Enter your lastname: ")
+    email = input("Enter your email: ")
+    street = input("Enter your street: ")
+    zip = input("Enter your zip: ")
+    city = input("Enter your city: ")
+
+    user_manager.register_guest(username, password, firstname, lastname, email, street, zip, city)
+    print("USERSTORY: Login")
+    while user_manager.has_attempts_left():
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        if user_manager.login(username, password):
+            print("Login successful!")
+            break
+        else:
+            print("Login failed! Try again!")
+
+    if user_manager.get_current_login():
+        print(f"Welcome {user_manager.get_current_login().username}")
+    else:
+        print("No attempts left, program is closed!")
+        sys.exit(1)
